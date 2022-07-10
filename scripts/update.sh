518ffeb985f8cb13da44ab1f62525665f4182873
Ggaast <gaast.dir@gmail.com>
2022-07-10 16:04:07 +0200

    Update

--- scripts/update.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p ./versions
-fetch --repo="https://github.com/ggaast/griffsort_game" --tag="~>0.0.0" --github-oauth-token="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" --progress --release-asset="linux.zip" ./versions
