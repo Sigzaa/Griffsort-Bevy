18086a02c08467f7bc4a16467536381d60c8c135
ggaast <givemilkyway@gmail.com>
2022-07-11 12:39:26 +0300

    Add downgrade to backup

--- build-assets/windows/update.bat
+++ build-assets/windows/update.bat
@@ -5,7 +5,7 @@ mkdir .buffer
 .\bundle\fetch.exe --repo="https://github.com/ggaast/griffsort_game" --tag="~>0.0.0" --github-oauth-token="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" --release-asset="win.zip" .buffer
 
 echo Creating Backup
-.\bundle\7z.exe a .\backup\back.zip Griffsort.exe config assets update.bat
+.\bundle\7z.exe a .\backup\back.zip Griffsort.exe config assets update.bat downgrade.bat
 
 
 echo Upgrading Griffsort
